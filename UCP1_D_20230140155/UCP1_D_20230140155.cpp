d0a2be33056cafa73effe67ac25c08098d8cdf76
Rangga1011 <ranggaramadhana1011@gmail.com>
2024-05-05 14:22:47 +0700

    Add project files.

---
+++ UCP1_D_20230140155/UCP1_D_20230140155.cpp
@@ -0,0 +1,134 @@
+//No. 1// Contoh 1 : int umur
+// COntoh 2 : string nama
+//Contoh 3 : float nilai
+
+//No. 2 
+// contoh prosedure : void display() {
+//cout << "Luas persegi : " << hitungluas() << endl;
+//}
+// fungsi : int main()
+//{
+//    inputdata();
+//    display();
+//}
+
+//NO. 3  //for (i = 60; i > 10; i -= 10) {
+//cout << i << "Selamat Pagi Dunia" << endl;
+    }
+  //  cout << "Nilai 1 terakhir :" << i << endl;
+
+// No. 4 //  int nBilangan;
+    //string status;
+
+    //srand(time(0));
+
+    //nBilangan = rand() % 6;
+
+    //if (nBilangan % 2 == 0) {
+    //    status = "Genap";
+  //  }
+    //else {
+      //  status = "Ganjil";
+   // }
+    //cout << "Bilangannya adalah " << nBilangan << endl;
+    //cout << "status bilangan adalah " << status << endl;
+
+   // return 0;
+	//  cout << "Nilai X : " << x << endl;
+//  }
+
+// N0. 5 // struct mahasiswa {
+//	string nim;
+//	string nama;
+//	DetailAlamat alamat;
+//	int umur;
+//};
+   // int main() {
+      //  mahasiswa  mhs;
+      //  cout << "nomor Mahasiswa : ";
+      //  cin >> mhs.nim;
+       // cout << "Nama Mahasiswa :";
+       // cin >> mhs.nama;
+       // cout << "alamat mahasiswa : ";
+       // cin >> mhs.alamat;
+       // cout << "Umur mahasiswa : ";
+       // cin >> mhs.umur;
+
+// No.  6 
+#include <iostream>
+    using namespace std;
+
+    int main()
+    {
+        int sensor1, sensor2, sensor3, ntingkatpolutan;
+        string status;
+        string NamaKota;
+
+        cout << "Masukan sendor1 : ";
+        cin >> sensor1;
+
+        cout << "Masukan sensor2 : ";
+        cin >> sensor2;
+
+        cout << "Masukan sensor3 : ";
+        cin >> sensor3;
+
+
+        if (ntingkat polutan > 75) {
+            status = "Sehat";
+        }
+        if (ntingkat polutan > 45) {
+            status = "Sehat";
+        }
+        else if (sensor1 > 70) {
+            status = "sehat";
+        }
+        else if (sensor2 > 70) {
+            status = "sehat";
+        }
+        else if (sensor3 > 45) {
+            status = "sehat";
+        }
+        else {
+            status = "sehat";
+        }
+        cout << "sensor1 : " << sensor1 << endl;
+        cout << "sensor2 : " << sensor2 << endl;
+        cout << "sensor3 : " << sensor3 << endl;
+        cout << "Status : " << ntingkatpolutan << endl;
+        cout << "NamaKota : " << nNamaKota << endl;
+
+        return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
